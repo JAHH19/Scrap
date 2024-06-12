b4a1d8e519de5abba0bd01e8cc505ec5e56dd8ac
Jesus Huaman Huaman <babymetal1995@gmail.com>
2024-06-12 13:52:52 -0500

    Cambios nuevos

--- scrap.py
+++ scrap.py
@@ -16,4 +16,4 @@ def generate_curl():
     return jsonify({'curl_command': curl_command})
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Ejecuta el servidor Flask en modo debug
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
