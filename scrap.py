8852079acaffd73e47afd3ddea1a6fca3318ec94
Jesus Huaman Huaman <babymetal1995@gmail.com>
2024-06-12 19:08:45 -0500

    sdasda

--- scrap.py
+++ scrap.py
@@ -14,7 +14,7 @@ def get_html_content(url: str) -> str:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Accept-Language': 'en-US,en;q=0.9',
-            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Encoding': 'deflate',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Connection': 'keep-alive',
             'Referer': url  # Agrega el encabezado Referer con la URL de origen
